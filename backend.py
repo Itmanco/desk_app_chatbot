4f169fccdda4c7d023f27d28b31372aaff6c4225
Jaime Corredor <itmanco@gmail.com>
2023-04-03 05:54:23 +0900

    openai key updated

---
+++ backend.py
@@ -0,0 +1,20 @@
+import openai
+import os
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = os.getenv("openai_key")
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=4000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("tell me something funny")
+    print(response)
